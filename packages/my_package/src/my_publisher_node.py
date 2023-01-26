1e7cc66d2d06b2e22c57a3d32021e83fb6b0ed06
friedballerina <aleksandra.ikonnikova@gmail.com>
2023-01-26 10:11:14 +0000

    started working on PID, first version on Proportional component

--- packages/my_package/src/my_publisher_node.py
+++ packages/my_package/src/my_publisher_node.py
@@ -6,6 +6,9 @@ from std_msgs.msg import String
 from smbus2 import SMBus
 from duckietown_msgs.msg import WheelsCmdStamped
 
+device_aadress = 62
+registry_address = 17
+
 speed = WheelsCmdStamped()
 
 class MyPublisherNode(DTROS):
@@ -23,44 +26,62 @@ class MyPublisherNode(DTROS):
         rospy.on_shutdown()
         
     def simple_track(self):
-        def correct_to_right():
-            speed.vel_right = 0.0
+        def steer_left():
             speed.vel_left = medium
-        def correct_to_left():
+            speed.vel_right = speed.vel_right * 0.1
+        def steer_right():
+            speed.vel_right = medium
+            speed.vel_left = speed.vel_right * 0.1
+        def turn_left():
             speed.vel_left = 0.0
             speed.vel_right = medium
+        def turn_right():
+            speed.vel_right = 0.0
+            speed.vel_left = medium
+        def forward():
+            speed.vel_left = medium   
+            speed.vel_right = medium
+        def proportional(index):
+            Kp = (index + 1) * 0.1 * 0.1
+            error = 4.5 - index + 1
+            P = Kp * error
+            speed.vel_right = medium + P
+            speed.vel_left = medium - P
         
         last_value = 0
-        rate = rospy.Rate(20) # 20Hz
+        rate = rospy.Rate(20) # 20Hz - code runs 20 times per second
+        # check https://answers.ros.org/question/264812/explanation-of-rospyrate/ 
         slow = 0.25
         medium = 0.50
         fast = 2
+
         while not rospy.is_shutdown():
             bus = SMBus(1)
-            read = bus.read_byte_data(62,17)
-
-            if read != 0:
-                last_value = read
+            read = bus.read_byte_data(device_aadress,registry_address)
             
+            bits_block = bin(read)[2:]
+            leading_zeros = 8 - len(bits_block)
+            bits = leading_zeros*'0' + bits_block
+            left, right = bits[:4], bits[4:]
+
+            print("binary read is", bits)
+            print("left is", left)
+            print("right is", right)
+
             if read == 0:
                 if last_value < 24:     # went left
-                    correct_to_right()
-                elif last_value >= 24:  # went right 
-                    correct_to_left()
+                    turn_right()
+                else:  # went right 
+                    turn_left()
+            if read != 0:
+                last_value = read          
 
-            if read == 8 or read == 16 or read == 24: # FORWARD
-                speed.vel_left = medium   
-                speed.vel_right = medium
-            elif 0 < read < 8 or read == 12:  # STEER LEFT 
-                speed.vel_left = medium
-                speed.vel_right = speed.vel_right * 0.1
-            elif 24 < read < 255:      # STEER RIGHT
-                speed.vel_right = medium
-                speed.vel_left = speed.vel_right * 0.1
-            """   else:
-                speed.vel_left = 0
-                speed.vel_right = 0 """
-
+            if left == '0001' or right == '1000': # is going forward
+                forward()
+            else:
+                if left != '0001' and right != '1000' and '1' in bits:
+                    proportional(bits.index('1')) # returns the first found match 
+       
             self.pub.publish(speed)
             rate.sleep()
             bus.close()
@@ -76,52 +97,3 @@ if __name__ == '__main__':
     node.run()
     # keep spinning
     rospy.spin()
-
-
- # previous:
-""" #!/usr/bin/env python3
-from smbus2 import SMBus
-import os
-import rospy
-from duckietown.dtros import DTROS, NodeType
-from std_msgs.msg import String
-
-class MyPublisherNode(DTROS):
-
-    # Open i2c bus 1 and read one byte from address 80, offset 0
-
-    def __init__(self, node_name):
-        # initialize the DTROS parent class
-        super(MyPublisherNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
-        # construct publisher
-        self.pub = rospy.Publisher('chatter', String, queue_size=10)
-
-    def run(self):
-        bus = SMBus(1)  #1
-        # publish message every 1 second
-        rate = rospy.Rate(1) # 1Hz
-        while not rospy.is_shutdown():
-            b = bus.read_byte_data(17, 62) #2
-            print(b)    #3
-            #message = "Hello from %s" % os.environ['VEHICLE_NAME']
-            #rospy.loginfo("Publishing message: '%s'" % message)
-            rospy.loginfo("byte data is: '%s'" % b)
-            #self.pub.publish(message)
-            self.pub.publish(b)
-            rate.sleep()
-        bus.close() # 4
-
-    
-
-if __name__ == '__main__':
-    # create the node
-    node = MyPublisherNode(node_name='my_publisher_node')
-    # run node
-
-   
-
-    node.run()
-    # keep spinning
-    rospy.spin()
-     """
-
