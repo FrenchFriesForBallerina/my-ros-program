7cd47aefdcfe1b945512fd9fc0821abdc95b2382
friedballerina <aleksandra.ikonnikova@gmail.com>
2023-01-26 17:29:43 +0000

    time 0.53  with 0.02 and 0.55

--- packages/my_package/src/my_publisher_node.py
+++ packages/my_package/src/my_publisher_node.py
@@ -8,6 +8,8 @@ from duckietown_msgs.msg import WheelsCmdStamped
 
 device_aadress = 62
 registry_address = 17
+target_sensor_position = 4.5
+sensor_reading_weights = [16, 8, 4, 2, -2, -4, -8, -16] 
 
 speed = WheelsCmdStamped()
 
@@ -43,21 +45,23 @@ class MyPublisherNode(DTROS):
             speed.vel_right = medium
 
         def proportional(index):
-            Kp = index * 0.03
-            # medium kiirusel: 0.50
-            # 0.8 tõmbleb ja eriti edasi ei liigu
-            # 0.08 enam-vähem, aga ka tõmbleb üsna palju ja sõidab vahetevahel kõrvalolevale rajale
-            # 0.01 ja 0.05 juba parem, aga sõidab ikka üsna siksakiliselt ja vahel eksib teisele rajale 
-            error = 3.5 - index
+            Kp = index * 0.02
+
+            # with 0.02 and speed 0.55: time 0.53 with errors
+            # with 0.022 and speed 0.55: time 0.54 with errors
+            # with 0.022 and speed 0.525: errors!
+            # with 0.02 and speed 0.525: time
+            
+            error = target_sensor_position - index
             P = Kp * error
-            speed.vel_right = slow + P
-            speed.vel_left = slow - P
+            speed.vel_right = medium + P
+            speed.vel_left = medium - P
         
         last_value = 0
         rate = rospy.Rate(20) # 20Hz - code runs 20 times per second
         # check https://answers.ros.org/question/264812/explanation-of-rospyrate/ 
-        slow = 0.35
-        medium = 0.50
+        slow = 0.16
+        medium = 0.55
         fast = 2
 
         while not rospy.is_shutdown():
@@ -68,11 +72,8 @@ class MyPublisherNode(DTROS):
             leading_zeros = 8 - len(bits_block)
             bits = leading_zeros*'0' + bits_block
             
+            
             left, right = bits[:4], bits[4:]
-
-            #print("binary read is", bits)
-            #print("left is", left)
-            #print("right is", right)
 
             if read == 0:
                 if last_value < 24:     # went left
@@ -82,29 +83,26 @@ class MyPublisherNode(DTROS):
             if read != 0:
                 last_value = read           
 
-            if (left == '0001' and right == '1000'): # is going forward                 print("forward")
-                forward()
-            else:
-                if '1' in bits:
-                    if '1' in right and '1' in left: 
-                        ri = right.index('1') + 1
-                        li = "".join(reversed(left)).index('1') + 1
-                        if ri < li:
-                            proportional(ri * -1) 
-                        else:
-                            proportional(li)       
-                    else:
-                        if '1' in right:
-                            proportional((right.index('1') + 1) * -1)
-                        else:
-                            proportional("".join(reversed(left)).index('1') + 1)
-                    #index = 3.5
-                    #ri = right.index('1') 
-                    #li = "".join(reversed(left)).index('1')
-                    #proportional(max(ri, li))
-                    print("correcting")
-                    print("BIT ", bits.index('1') + 1)
-                    #proportional(bits.index('1')) # returns the first found match 
+            # calculate deviation from the centre
+            # find ones in bits
+            indices = []
+            for idx, value in enumerate(bits):
+                if value == '1':
+                    indices.append(idx + 1)
+            
+            print(indices)
+            
+            sum = 0
+            shift = 0
+            for i in indices:
+                sum += i
+            if len(indices) != 0:
+                shift = sum / (len(indices)) * 1.0
+
+                if (left == '0001' and right == '1000'): # is going forward                 print("forward")
+                    forward()
+                else:
+                    proportional(shift)
 
             self.pub.publish(speed)
             rate.sleep()
@@ -121,3 +119,5 @@ if __name__ == '__main__':
     node.run()
     # keep spinning
     rospy.spin()
+
+    # https://se.mathworks.com/help/supportpkg/arduino/ref/arduino-robot-line-follower-application.html
