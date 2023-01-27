b16941accebbd63a4b0be8b5f19125da48672467
friedballerina <aleksandra.ikonnikova@gmail.com>
2023-01-27 13:23:47 +0000

    P, I and D draft done

--- packages/my_package/src/my_publisher_node.py
+++ packages/my_package/src/my_publisher_node.py
@@ -8,9 +8,18 @@ from duckietown_msgs.msg import WheelsCmdStamped
 device_aadress = 62
 registry_address = 17
 target_sensor_position = 4.5
-sensor_reading_weights = [16, 8, 4, 2, -2, -4, -8, -16] 
+vehicle_speed = 0.55
+rospy_rate = 20
+
+# constants:
+Kp = 0.1
+Ki = 0
+Kd = 0.1
+I = 0
 
 speed = WheelsCmdStamped()
+error = 0
+last_error = 0
 
 class MyPublisherNode(DTROS):
     
@@ -27,24 +36,17 @@ class MyPublisherNode(DTROS):
         rospy.on_shutdown()
         
     def simple_track(self):
-        def steer_left():
-            speed.vel_left = medium
-            speed.vel_right = speed.vel_right * 0.1
-        def steer_right():
-            speed.vel_right = medium
-            speed.vel_left = speed.vel_right * 0.1
         def turn_left():
             speed.vel_left = 0.01
-            speed.vel_right = medium
+            speed.vel_right = vehicle_speed
         def turn_right():
             speed.vel_right = 0.01
-            speed.vel_left = medium
+            speed.vel_left = vehicle_speed
         def forward():
-            speed.vel_left = medium   
-            speed.vel_right = medium
+            speed.vel_left = vehicle_speed   
+            speed.vel_right = vehicle_speed
 
-        def proportional(index):
-            Kp = 0.1
+        def find_proportional_gain(error):
             # kp 0.1 speed 0.55, time 0.52.10
             # kp 0.05 speed 0.55, time 0.53.14
             # kp 0.05 speed 0.6, time 0.53 with errors
@@ -55,16 +57,29 @@ class MyPublisherNode(DTROS):
             # kp 0.1 speed 0.53, time - errory
             # kp 0.1 speed 0.5, time - error, not too bad, 0.59
 
-            error = target_sensor_position - index
             P = Kp * error
-            speed.vel_right = medium + P
-            speed.vel_left = medium - P
+            return P
+
+        def find_integral_gain(error):
+            global I
+            I = I + (rospy_rate * error * Ki)
+            # print("integraL:", I)
+            return I
+
+        def find_derivative_gain(error, last_error):
+            D = Kd * ((error - last_error)/rospy_rate)
+            # print("integraL:", I)
+            return D
+
+        def apply_PI(P):
+            speed.vel_right = vehicle_speed + P
+            speed.vel_left = vehicle_speed - P
+        
+        global error
+        global last_error
         
         last_value = 0
-        rate = rospy.Rate(20) # 20Hz - code runs 20 times per second
-        slow = 0.16
-        medium = 0.55
-        fast = 2
+        rate = rospy.Rate(rospy_rate)
 
         while not rospy.is_shutdown():
             bus = SMBus(1)
@@ -92,16 +107,30 @@ class MyPublisherNode(DTROS):
             print(indices)
             
             sum = 0
-            shift = 0
+            average = 0
+
             for i in indices:
                 sum += i
             if len(indices) != 0:
-                shift = sum / (len(indices)) * 1.0
+                average = sum / (len(indices)) * 1.0
+                print("Error:", error)
+                
+                last_error = error
+                error = target_sensor_position - average
+
+                print("Error:", error)
+                print("Last error:", last_error)
 
                 if (left == '0001' and right == '1000'): # is going forward                 print("forward")
                     forward()
                 else:
-                    proportional(shift) # if not going forward, then correct
+                    P = find_proportional_gain(error)
+                    I = find_integral_gain(error)
+                    D = find_derivative_gain(error, last_error)
+
+                    print("Integral gain is", I)
+                    print("Derivative gain is", D)
+                    apply_PI(P+I+D) # if not going forward, then correct
 
             self.pub.publish(speed)
             rate.sleep()
@@ -120,3 +149,4 @@ if __name__ == '__main__':
 
 # https://se.mathworks.com/help/supportpkg/arduino/ref/arduino-robot-line-follower-application.html
 # check https://answers.ros.org/question/264812/explanation-of-rospyrate/ 
+# https://www.youtube.com/watch?v=wkfEZmsQqiA 
