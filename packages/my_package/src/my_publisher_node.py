26ad53e379bcad63274d1fd43e9768545dbe3d8b
friedballerina <aleksandra.ikonnikova@gmail.com>
2023-02-03 10:30:56 +0000

    saves log files about rostopic variable

--- packages/my_package/src/my_publisher_node.py
+++ packages/my_package/src/my_publisher_node.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
-import rospy
+import os, rospy
+from csv import writer
+import subprocess
+
 from duckietown.dtros import DTROS, NodeType
 from std_msgs.msg import String
 from smbus2 import SMBus
 from duckietown_msgs.msg import WheelsCmdStamped
 
-device_aadress = 62
-registry_address = 17
+sparkfun_device_aadress = 62
+sparkfun_registry_address = 17
 target_sensor_position = 4.5
-vehicle_speed = 0.55
+vehicle_speed = 0.4
 rospy_rate = 20
 
 # constants:
-Kp = 0.1
-Ki = 0
-Kd = 0.1
+Kp = 0.05 # 0.065 # used to be o.1
+Ki = 0 # 0.0001 pane piirangud peale
+Kd = 0.02 #0.04
 I = 0
 
 speed = WheelsCmdStamped()
 error = 0
 last_error = 0
+
+def update_csv(lap_time, vehicle_speed, rate, P, I, D):
+    print("writing into file")
+    List = [lap_time, vehicle_speed, rate, P, I, D]
+    with open('results.csv', 'a') as f_object:
+        writer_object = writer(f_object)
+        writer_object.writerow(List)
+        f_object.close()
 
 class MyPublisherNode(DTROS):
     
@@ -36,72 +47,48 @@ class MyPublisherNode(DTROS):
         rospy.on_shutdown()
         
     def simple_track(self):
-        def turn_left():
-            speed.vel_left = 0.01
-            speed.vel_right = vehicle_speed
-        def turn_right():
-            speed.vel_right = 0.01
-            speed.vel_left = vehicle_speed
-        def forward():
-            speed.vel_left = vehicle_speed   
-            speed.vel_right = vehicle_speed
+        #location = os.environ['REPO_NAME']
+        #print(location)
+        #c691e634dc2a
 
-        def find_proportional_gain(error):
-            # kp 0.1 speed 0.55, time 0.52.10
-            # kp 0.05 speed 0.55, time 0.53.14
-            # kp 0.05 speed 0.6, time 0.53 with errors
-            # kp 0.1 speed 0.6, time - lots of errors, too zigzaggy
-            # kp 0.075 speed 0.6, time - lots of errors
-            # kp 0.075 speed 0.55, time - errors, wheel gets stuck
-            # kp 0.08 speed 0.55, time - one error - goes onto neighbor lane
-            # kp 0.1 speed 0.53, time - errory
-            # kp 0.1 speed 0.5, time - error, not too bad, 0.59
-
-            P = Kp * error
-            return P
-
-        def find_integral_gain(error):
-            global I
-            I = I + (rospy_rate * error * Ki)
-            # print("integraL:", I)
-            return I
-
-        def find_derivative_gain(error, last_error):
-            D = Kd * ((error - last_error)/rospy_rate)
-            # print("integraL:", I)
-            return D
-
-        def apply_PI(P):
-            speed.vel_right = vehicle_speed + P
-            speed.vel_left = vehicle_speed - P
-        
         global error
         global last_error
-        
         last_value = 0
         rate = rospy.Rate(rospy_rate)
 
+        def turn_left():
+            speed.vel_left = 0.001 * vehicle_speed
+            speed.vel_right = vehicle_speed
+        def turn_right():
+            speed.vel_right = 0.001 * vehicle_speed
+            speed.vel_left = vehicle_speed 
+        def forward():
+            speed.vel_left = vehicle_speed  
+            speed.vel_right = vehicle_speed
+
+        def apply_controller(error, last_error):
+            global I
+            P = Kp * error
+            I = I + (rospy_rate * error * Ki)
+            D = Kd * ((error - last_error)/rospy_rate)
+            PID = P + I + D
+            speed.vel_right = vehicle_speed + PID
+            speed.vel_left = vehicle_speed - PID
+        
         while not rospy.is_shutdown():
             bus = SMBus(1)
-            read = bus.read_byte_data(device_aadress,registry_address)
-            
-            bits_block = bin(read)[2:]
+            read = bus.read_byte_data(sparkfun_device_aadress,sparkfun_registry_address)
+            print('read is', read)       
+            bits_block = bin(read)[2:] #'01100000'
             leading_zeros = 8 - len(bits_block)
             bits = leading_zeros*'0' + bits_block
             
-            left, right = bits[:4], bits[4:]
-
-            if read == 0:
-                if last_value < 24:     # went left
-                    turn_right()
-                else:                   # went right 
-                    turn_left()
-            if read != 0:
-                last_value = read           
+            #left, right = bits[:4], bits[4:]      
 
             indices = []
-            for idx, value in enumerate(bits):
+            for idx, value in enumerate(bits): 
                 if value == '1':
+                    print('bit index is', idx)
                     indices.append(idx + 1)
             
             print(indices)
@@ -111,26 +98,33 @@ class MyPublisherNode(DTROS):
 
             for i in indices:
                 sum += i
+
+            if bits_block == '00000000':
+                print('OFF ROAD')
+                if 1 in last_value[4:]:    
+                    turn_right()
+                    print('turning right')
+                else:                   
+                    turn_left()
+                    print('turning left')
+
+            if bits_block != '00000000':
+                last_value = read   
+
             if len(indices) != 0:
                 average = sum / (len(indices)) * 1.0
-                print("Error:", error)
                 
                 last_error = error
                 error = target_sensor_position - average
 
-                print("Error:", error)
-                print("Last error:", last_error)
-
-                if (left == '0001' and right == '1000'): # is going forward                 print("forward")
+                if bits_block == '00011000': # is going forward                 print("forward")
                     forward()
                 else:
-                    P = find_proportional_gain(error)
-                    I = find_integral_gain(error)
-                    D = find_derivative_gain(error, last_error)
+                    apply_controller(error, last_error) # if not going forward, then correct
 
-                    print("Integral gain is", I)
-                    print("Derivative gain is", D)
-                    apply_PI(P+I+D) # if not going forward, then correct
+            print("update csv values", 0, vehicle_speed, rospy_rate, Kp, Ki, Kd)    
+            update_csv(0, 0, 0, Kp, Ki, Kd)
+
 
             self.pub.publish(speed)
             rate.sleep()
